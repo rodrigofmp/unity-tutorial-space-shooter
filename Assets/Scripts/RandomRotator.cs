3aad6ee18d648b12936341cb690cca5b1e17c351
Rodrigo Petters <rodrigo.petters@inovan.net>
2017-02-08 23:29:20 -0200

    Encerrado

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour {
+
+	public float tumble;
+
+	// Use this for initialization
+	void Start () {
+		Rigidbody rigidbody = GetComponent<Rigidbody> ();
+		rigidbody.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+	
+
+}
